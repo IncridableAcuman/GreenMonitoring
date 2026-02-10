89ec3855413db5205569209e49a9d8178dd1063d
Izzatbek <abdusharipovizzat03@gmail.com>
2026-02-10 21:31:41 +0500

    IoT Monitoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import {Routes,Route} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import ResetPassword from './pages/ResetPassword';
 const App = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/auth' element={<Auth/>} />
+      <Route path='/reset-password' element={<ResetPassword/>} />
     </Routes>
     </>
   )
--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -1,8 +1,71 @@
+import { Eye, EyeClosed, Lock } from "lucide-react";
+import { useState } from "react";
 
 const ResetPassword = () => {
-  return (
-    <div>ResetPassword</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [confirmShowPassword,setConfirmShowPassword] = useState(false);
 
-export default ResetPassword
+  return (
+    <div className="bg_image h-screen w-full flexing_style2">
+      <div className="bg-gray-950 text-white w-full max-w-xl p-8 rounded-md shadow-md text-center">
+        <h1 className="text-2xl md:text-3xl font-semibold py-4">
+          Change Your Password
+        </h1>
+        <p className="pb-8">
+          Enter a new password below to change your password.
+        </p>
+        <form className="space-y-4">
+          <div className="flex items-center gap-2 border border-gray-800 bg-gray-900 p-3 rounded-md">
+            <Lock />
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder="New Password"
+              className="w-full outline-none bg-transparent"
+            />
+            {showPassword ? (
+              <Eye
+                className="cursor-pointer"
+                onClick={() => setShowPassword(false)}
+              />
+            ) : (
+              <EyeClosed
+                className="cursor-pointer"
+                onClick={() => setShowPassword(true)}
+              />
+            )}
+          </div>
+          <div className="flex items-center gap-2 border border-gray-800 bg-gray-900 p-3 rounded-md">
+            <Lock />
+            <input
+              type={confirmShowPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder="Re-enter new password"
+              className="w-full outline-none bg-transparent"
+            />
+            {confirmShowPassword ? (
+              <Eye
+                className="cursor-pointer"
+                onClick={() => setConfirmShowPassword(false)}
+              />
+            ) : (
+              <EyeClosed
+                className="cursor-pointer"
+                onClick={() => setConfirmShowPassword(true)}
+              />
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-linear-to-r from-pink-500 to-purple-600 p-3 rounded-md shadow-md cursor-pointer
+           hover:bg-linear-to-r hover:from-purple-600 hover:to-pink-500 transition duration-300"
+          >Reset Password</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
